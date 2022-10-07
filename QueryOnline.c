c5d99eb8bdf7cf1f66090baf81583b0217ed32b7
azalitam <azahara.martinez@estudiantat.upc.edu>
2022-10-07 10:24:19 +0200

    Primera version

---
+++ QueryOnline.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+//programa en C para consultar los datos de la base de datos
+//Incluir esta libreria para poder hacer las llamadas en shiva2.upc.es
+//#include <my_global.h>
+#include <mysql.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	MYSQL *conn;
+	int err;
+	MYSQL_RES *resultado;
+	MYSQL_ROW row;
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	} //Crea la conexion a la base de datos, retorna 1 si no puede.
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "Juego", 0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	} //Inntenta iniciar conexion recien creada, retorna 1 si no pude.
+	err=mysql_query (conn, "SELECT Usuarios.nombre FROM (Usuarios) WHERE Usuarios.Estado= 'Online' ");
+	if (err!=0) {
+		printf ("Error al consultar datos de la base %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	} //Hace la consulta (Quien online?), 1 si no puede.
+	resultado = mysql_store_result (conn);
+	row = mysql_fetch_row (resultado);
